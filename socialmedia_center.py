d2ed3830698ecd76a56f7bf37d599024335365f2
OpenSoftware-World <umutarda863@gmail.com>
2026-07-10 01:00:04 +0300

    Unnecessary code has been removed, and OpenSoftware-World SocialMediaCenter has been optimized as intended.

--- socialmedia_center.py
+++ socialmedia_center.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
